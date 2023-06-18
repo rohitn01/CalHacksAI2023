92efaae775b27d9ac146d245e9854e7edab8c8d6
Rohit Narayanan <rohitmen01@gmail.com>
2023-06-18 00:28:18 -0700

    implemented pdf uploading, need to test

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from gpt import *
+
+
+UPLOAD_FOLDER = './pdf_uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
 
@@ -28,6 +34,34 @@ def get_response():
         prompt_response = question_dict
     inital_prompts_filled += 1
     return {'response': prompt_response}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+        return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     inital_prompts_filled = 0
