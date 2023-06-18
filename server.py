1f8bc0d947aa70f44ea5cc9287214d1ab001eb02
Rohit Narayanan <rohitmen01@gmail.com>
2023-06-18 10:19:44 -0700

    added file upload functionality

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
