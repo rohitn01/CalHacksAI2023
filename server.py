a1c45d12f9efa759dd461510575a486a9188bc2a
Sumedh Vaidyanathan <sumedh@pulsepoint.org>
2023-06-18 03:43:02 -0700

    Showing multiple questions in a row

--- server.py
+++ server.py
@@ -8,10 +8,26 @@ inital_prompts_filled = 0
 user_class = None
 user_topic = None
 user_q_count = None
+answers = []
+questions = None
+R = None
+curr_question_response = 1
+gpt_quiz_feedback = None
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+'''
+@app.route('/update_chat', methods=['POST'])
+def update_chat():
+    chat_data = request.json.get('chatData')  # Assuming the frontend sends JSON data
+    
+    for i in range(len(chat_data) - 1, len(chat_data) - 1 - len(answers), -1):
+    chat_data.insert(i + 1, )
+    
+    return jsonify({'status': 'success', 'chatData': chat_data})
+'''
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
@@ -21,6 +37,9 @@ def get_response():
     global user_q_count
     global questions
     global answers
+    global curr_question_response
+    global R
+    global gpt_quiz_feedback
 
     user_message = request.form['user_message']
     if inital_prompts_filled == 0:
@@ -38,10 +57,23 @@ def get_response():
         print(response_json)
         response = json.loads(response_json)
         questions = response["questions"]
-        prompt_response = response["summary"] + "\n" + questions[0]
-    elif inital_prompts_filled == 4:
+        prompt_response = response["summary"] + "\n\n" + questions[0]
+    elif inital_prompts_filled > 3:
         answers.append(user_message)
-        prompt_response = questions[1]
+        curr_question_response += 1
+
+        if(curr_question_response <= len(questions)):
+            prompt_response = questions[curr_question_response - 1]
+        else:
+            '''
+            Q, R = get_answer(R, answers)
+            response_json = chatGPT(Q + R)
+            response = json.loads(response_json)
+            gpt_quiz_feedback = response["answers"]
+            '''
+            pass
+
+
 
     inital_prompts_filled += 1
     return {'response': prompt_response}
