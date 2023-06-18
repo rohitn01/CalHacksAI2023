92a932663d83df9ad398d5c2ecade4fc528b50be
Sumedh Vaidyanathan <sumedh@pulsepoint.org>
2023-06-18 00:11:20 -0700

    Fixed gpt_client.py

---
+++ gpt_client.py
@@ -0,0 +1,59 @@
+import openai
+import json
+
+openai.api_key = "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"
+
+def chatGPT(prompt, model_engine="text-davinci-003", max_tokens = 1024):
+  completion = openai.Completion.create(
+      engine=model_engine,
+      prompt=prompt,
+      max_tokens=max_tokens,
+      temperature=0.3,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+      )
+  return completion.choices[0].text
+
+def get_question(class_name, topic, num_q):
+  question_prompt = "Acting as a friend in my college class called " + class_name + ", " +\
+  "engage with me in constructive discussion and exmplain in simple and understandable terms what " + topic +\
+  " is in two sentences, and generate " + str(num_q) + " short answer question" + ("s" if num_q > 1 else "") + " to test my understanding."
+  tone_prompt = "Use a friendly tone and stimulate my intellectual curiosity."
+  formatting_prompt = '''
+  Return the response in the following JSON format (only return JSON and do not put any plain text in the response): 
+    {
+      "summary": <the summary>,
+      "questions" : [
+        <question1>,
+        <question2>,
+      ]
+    }
+    '''
+  return (question_prompt + "\n" + tone_prompt, formatting_prompt)
+
+def get_answer(R, answers):
+  intro_prompt = "Given that you just asked the user these questions: "
+  answer_prompt = "And the user responds to each question according to the following:"
+  for i, ans in enumerate(answers):
+    answer_prompt += "\n"
+    answer_prompt += "Answer to question " + str(i) + ": "
+    answer_prompt += '"' + ans + '"'
+  generate_prompt = "Critically evaluate and break down the user's response in a constructive manner. For the correctness rating put either 'Correct', 'Partially Correct', or 'Incorrect'. Do not just give the right answer. Try to explore further implications of the question but keep responses to five sentences or fewer."
+  tone_prompt = '''Speak from a second person perspective, address to the user as 'you' and the user's response as 'your response', and ensure your responses to each question are contained in each 'response' field.
+  Don't speak in a robotic tone, speak like a friendly tutor who has a secret crush on you and be VERY conversational.
+  '''
+  format_prompt = '''Return the response in the following JSON format (only return JSON and do not put any plain text in the response): 
+  {"answers": [
+    {
+      "rating": <put the correctness rating to the first question here>
+      "explanation": <put the response to the first question here>
+    },
+    {
+      "rating": <put the correctness rating to the second question here>
+      "explanation": <put the response to the second question here>
+    },
+  ]
+  }'''
+  prompt = intro_prompt + '\n"' + R + '"\n' + answer_prompt + "\n" + generate_prompt + '\n' + tone_prompt + '\n' + format_prompt
+  return prompt
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
-from gpt import *
+from gpt_client import chatGPT, get_question, get_answer
 
 app = Flask(__name__)
-
-
+inital_prompts_filled = 0
+user_class = None
+user_topic = None
+user_q_count = None
 
 @app.route('/')
 def index():
@@ -11,6 +13,11 @@ def index():
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
+    global inital_prompts_filled
+    global user_class
+    global user_topic
+    global user_q_count
+
     user_message = request.form['user_message']
     if inital_prompts_filled == 0:
         prompt_response = "Which class?"
@@ -21,11 +28,9 @@ def get_response():
         user_topic = user_message
         prompt_response = "How many questions?"
     elif inital_prompts_filled == 3:
-        user_q_count = user_message
+        user_q_count = int(user_message)
         question_prompt = get_question(user_class, user_topic, user_q_count)
-        json_response = chatGPT(question_prompt)
-        question_dict = json.load(json_response)
-        prompt_response = question_dict
+        prompt_response = chatGPT(question_prompt)
     inital_prompts_filled += 1
     return {'response': prompt_response}
 
