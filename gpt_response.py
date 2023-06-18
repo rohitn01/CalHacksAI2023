1851654ed24b527d0397f3f5d5f7bef2198c7541
Rohit Narayanan <rohitmen01@gmail.com>
2023-06-17 21:16:11 -0700

    implemented parsing

---
+++ gpt_response.py
@@ -0,0 +1,10 @@
+class GPTResponse(self):
+    def __init__(self, summary, questions):
+        self.summary = summary
+        self.questions = input
+
+    def get_summary(self):
+        return self.summary
+    
+    def get_questions(self):
+        return self.questions
