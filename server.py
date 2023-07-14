dedc9fe490751eb86b26837e04b731c97ce6af3e
Sumedh Vaidyanathan <sumedh@pulsepoint.org>
2023-07-14 01:38:55 -0700

    Functional website :)

--- server.py
+++ server.py
@@ -6,13 +6,13 @@ from gpt_client import chatGPT, get_question, get_answer
 from markupsafe import escape
 import json
 
-
-# CHANGE WHEN PRESENTING FOR UPLOAD FOLDER
 UPLOAD_FOLDER = str(os.getcwd()) + '/pdf_uploads'
+SECRET_KEY = "secret-key"
 ALLOWED_EXTENSIONS = {'pdf'}
 NUM_QUESTIONS_ASKED = 5
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 inital_prompts_filled = 0
@@ -33,144 +33,86 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/generate-reviews', methods=['GET', 'POST'])
-def handle_human_answers():
-       questions = request.form['questions']
-       answers = request.form['answers']
-
-       gpt_chat = get_answer(questions, answers)
-       response_json = chatGPT(gpt_chat)
-       print("afhbjafja", response_json)
-       response = json.loads(response_json)
-       
-       return render_template("return-results.html", questions=questions, answers=answers, gpt_reviews=response["answers"], zip=zip)
-
-@app.route('/generate-questions', methods=['GET', 'POST'])
-def generate_questions():
-    global user_topic
+@app.route('/error')
+def error():
+    return render_template('page_not_found.html')
+    
+@app.route('/questions', methods=['GET', 'POST'])
+def questions():
+    
+    #global user_topic
     global user_class
     global NUM_QUESTIONS_ASKED
     global R
     global Q
-    if request.method == 'POST':
+    
+    if request.method == "POST":
         user_topic = request.form['topic']
-
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        
-        file = request.files['file']
+        file = request.files['pdf']
 
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return redirect(url_for("error"))
         
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('download_file', name=filename))
-        
-
+            file.save(filename)
         
         #Embedding logic with FeatureForm must be connected
-        
-        user_class = "Default Class"
-        
+        '''
+        user_class = "Default Class"   
         Q, R = get_question(user_class, user_topic, NUM_QUESTIONS_ASKED)
         response_json = chatGPT(Q + R)
         print(response_json)
         response = json.loads(response_json)
         questions = response["questions"]
-        return render_template('ask-users.html', questions=questions)
+        '''
+        
+        #default questions for display sake. Comment out when embedding logic works
+        questions = [
+    
+        "What was the percentage drop of African-American owned businesses due to the pandemic?",
+        "What was the percentage drop of immigrant-owned businesses due to the pandemic?"  ]
+    
+        return render_template('questions.html', questions=enumerate(questions))
 
-'''
-@app.route('/generate-questions/<request>', methods=['POST', 'GET'])
-def test(request):
-    print("Workig sg")
-    ex = [
-    "What is the smallest unit of life?",
-    "What processes do cells carry out?",
-    "What are cells made of?",
-    "How do cells work together to make life possible?",
-    "What are the different types of cells?"
-  ]
-    return render_template("ask-users.html", questions=ex)
-'''
+@app.route('/reveal_answers', methods=['GET', 'POST'])
+def reveal_answers():
+       global NUM_QUESTIONS_ASKED
+       if request.method == "POST":           
+            questions = []
+            answers = []
+            for key, value in request.form.items():
+                if key.startswith("question"):
+                    questions.append(value)
+                elif key.startswith("answer"):
+                    answers.append(value)
+                print("Key: ", key, " Value: ", value)
+            if len(questions) == 0 or len(answers) == 0:
+                return redirect(url_for("error"))
 
-
-'''
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    global inital_prompts_filled
-    global user_class
-    global user_topic
-    global user_q_count
-    global questions
-    global answers
-    global curr_question_response
-    global R
-    global Q
-    global gpt_quiz_feedback
-
-    user_message = escape(request.form['user_message'])
-    if inital_prompts_filled == 0:
-        prompt_response = "Which class?"
-    elif inital_prompts_filled == 1:
-        user_class = user_message
-        prompt_response = "What topic?"
-    elif inital_prompts_filled == 2:
-        user_topic = user_message
-        prompt_response = "How many questions?"
-    elif inital_prompts_filled == 3:
-        user_q_count = int(user_message)
-        Q, R = get_question(user_class, user_topic, user_q_count)
-        response_json = chatGPT(Q + R)
-        print(response_json)
-        response = json.loads(response_json)
-        questions = response["questions"]
-        prompt_response = response["summary"] + "\n\n" + questions[0]
-    elif inital_prompts_filled > 3:
-        answers.append(user_message)
-        curr_question_response += 1
-
-        if(curr_question_response <= len(questions)):
-            prompt_response = questions[curr_question_response - 1]
-        else:
-            gpt_chat = get_answer(Q, answers)
+            # GPT Chat logic for getting answer
+            '''
+            gpt_chat = get_answer(questions, answers)
             response_json = chatGPT(gpt_chat)
             print("afhbjafja", response_json)
             response = json.loads(response_json)
-            gpt_quiz_feedback = response["answers"]
-            #prompt_response = gpt_quiz_feedback
-            prompt_response = "Provide feedback"
-
-
-
-    inital_prompts_filled += 1
-    return {'response': prompt_response}
-'''
-
-'''
-@app.route('/update_chat', methods=['POST'])
-def update_chat():
-    global gpt_quiz_feedback
-    global answers
-    global questions
-    chat_data = request.json.get('chatData')  
-    print("Lengeth chat data:", len(chat_data))
-    updated_chat_data = []
-    new_answers = [chat_data[i]  for i in range(len(chat_data) + 1 - (2 * len(answers)), len(chat_data), 2)]
-    new_questions = [chat_data[i] for i in range(len(chat_data) - (2 * len(questions)), len(chat_data), 2)]
-    for ques in range(questions):
-        updated_chat_data.append(new_questions[ques])
-        updated_chat_data.append(new_answers[ques])
-        updated_chat_data.append(gpt_quiz_feedback[ques]["rating"])
-        updated_chat_data.append(gpt_quiz_feedback[ques]["explanation"])
-
-    chat_data = chat_data[0:len(chat_data) - (2 * len(answers))].extend(updated_chat_data)
-    
-    return {'status': 'success', 'chatData': chat_data}
-'''
+            '''
+       
+            #default questions for display sake. Comment out when GPT Chat logic works
+            questions = [
+            "What was the percentage drop of African-American owned businesses due to the pandemic?",
+            "What was the percentage drop of immigrant-owned businesses due to the pandemic?"  ]
+            answers = ["67", "80"]
+            response = {"answers": [{
+            "rating": "Partially Correct",
+            "explanation": "Your response that African-American businesses saw a 67% drop is partially correct. The paper explains that the drop in African-American businesses was actually a percent drop, which is larger than your response. However, your response does provide indication that African-American business owners were hit especially hard. "
+            },
+            {
+            "rating": "Incorrect",
+            "explanation": "Your response that Latinx business owners saw an 80% drop is incorrect. According to the paper, Latinx business owners actually experienced a percent drop, which is lower than your response. Additionally, your response for the Asian business owners drop was also incorrect - the paper reports that Asian business owners actually dropped by percent, which is lower than your response. "
+            }]}
+        
+            return render_template("return-results.html", questions=questions, answers=answers, gpt_reviews=response["answers"], zip=zip)
 
 if __name__ == '__main__':
     inital_prompts_filled = 0
